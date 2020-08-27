c0b34d2e22cd11cf64a3216f0b34b4ce7e3af758
amitrai5100#gmail.com <amitrai5100@gmail.com>
2020-08-27 16:50:04 +0530

    amit ria

--- backend/seo/src/main/java/com/seo/controller/MainServices.java
+++ backend/seo/src/main/java/com/seo/controller/MainServices.java
@@ -1,4 +1,4 @@
- package com.seo.controller;
+package com.seo.controller;
 
 import java.util.Iterator;
 import java.util.List;
@@ -6,8 +6,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.seo.dto.Login;
 import com.seo.model.SignUp;
@@ -15,13 +17,13 @@ import com.seo.services.ISignUpService;
 
 @RestController
 public class MainServices {
-	
+
 	@Autowired
 	ISignUpService iSignUpService;
 
 	@Autowired
 	private  Environment env;
-		
+
 	@CrossOrigin(origins = "*")
 	@PostMapping("/signup")
 	public String showsignUp (@RequestBody SignUp  signup) {
@@ -35,43 +37,58 @@ public class MainServices {
 			}		
 			iSignUpService.save(signup);
 		} catch (Exception e) {
-			// TODO: handle exception 
+			e.printStackTrace(); 
 		}
-	
-
-		 return "{\"message\":\"Thank you registration\"}";
+		return "{\"message\":\"Thank you registration\"}";
 	}
-	
+
+
 	@CrossOrigin(origins = "*")
-    @PostMapping("/login")
-	 String view1(@RequestBody Login login) {
-		System.out.println("--------------"+ login.getMobileNumber());
-		System.out.println("--------------"+ login.getPassword());
+	@PostMapping("/forgetPassword")
+	public String showsignUp (@RequestBody Login login) {
+		try {
+			List<SignUp> signup = (List<SignUp>) iSignUpService.findAll();
+			System.out.println(signup.toString());
+			for (int i = 0; i < signup.size(); i++) {
+				if(signup.get(i).getPhoneNumber() == null ||signup.get(i).getPassword() == null ) {
+					continue;
+				}
+				if(signup.get(i).getPhoneNumber().equalsIgnoreCase(login.getMobileNumber()) ) {
+					return "{\"message\":\"Please check your Phone for OTP\"}";
+				}
+			}
+			return "{\"message\":\"User do not exist, Please create account or contact to administrator\"}";
+
+		} catch (Exception e) {
+			return "{\"message\":\"Error in sending message , Please try again \"}";
+		}
+	}
+
+
+	@CrossOrigin(origins = "*")
+	@GetMapping("/validateotp")
+	public String validateotp (@RequestParam(defaultValue="") String otp) {
+		try {
+			return "{\"message\":\"Otp Validation done suscessfully\"}";
+		} catch (Exception e) {
+			return "{\"message\":\"Error in sending message , Please try again \"}";
+		}
+	}
+
+	@CrossOrigin(origins = "*")
+	@PostMapping("/login")
+	String view1(@RequestBody Login login) {
 		List<SignUp> signup = (List<SignUp>) iSignUpService.findAll();
 		System.out.println(signup.toString());
 		for (int i = 0; i < signup.size(); i++) {
 			if(signup.get(i).getPhoneNumber() == null ||signup.get(i).getPassword() == null ) {
 				continue;
 			}
-			System.out.println(signup.get(i).getPhoneNumber());
-			System.out.println(signup.get(i).getPhoneNumber().equalsIgnoreCase(login.getMobileNumber()));
-			System.out.println(signup.get(i).getPassword().equals(login.getPassword()));
 			if(signup.get(i).getPhoneNumber().equalsIgnoreCase(login.getMobileNumber()) && signup.get(i).getPassword().equals(login.getPassword()) ) {
 				return signup.get(i).toString();
 			}
 		}
-        return "{\"message\":\"User do not exist, Please create account or contact to administrator\"}";
+		return "{\"message\":\"User do not exist, Please create account or contact to administrator\"}";
 	}
-	
-	
-	
-		
-//		@CrossOrigin(origins = "*")
-//		@GetMapping("/getquestionofsurveyforPredefine")
-//		public String getquestionofsurveyforPredefine (@RequestParam(defaultValue="") String type ) {
-//			return com.erpo.bean.Bean.getquestionofsurveyforPredefine(type);
-//		}
-		
-		
-		
-		}
+}
+
--- backend/seo/src/main/java/com/seo/model/SignUp.java
+++ backend/seo/src/main/java/com/seo/model/SignUp.java
@@ -17,25 +17,92 @@ public class SignUp {
 	private String phoneNumber;
 	private String email;
 	private String password;
+	private String profession;
+	private String organisation;
+	
+	public SignUp(long id, String name, String phoneNumber, String email, String password, String profession,
+			String organisation, String role, boolean loginstatus, String submissionEmailID,
+			String submissionPassword) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.password = password;
+		this.profession = profession;
+		this.organisation = organisation;
+		this.role = role;
+		this.loginstatus = loginstatus;
+		this.submissionEmailID = submissionEmailID;
+		this.submissionPassword = submissionPassword;
+	}
+
+	private String role;
+	private boolean loginstatus;
+	private String submissionEmailID;
+	
+	@Override
+	public String toString() {
+		return "{\"id\":" + id + ", \"name\":\"" + name + ",\"organisation\":\"" + organisation + "\", \"email\":\"" + email + "\", \"submissionEmailID\":\"" + submissionEmailID
+				+ "\",\"profession\":\""+ profession +  "\"}";
+	}
+	public String getProfession() {
+		return profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+
+	public String getOrganisation() {
+		return organisation;
+	}
+
+	public void setOrganisation(String organisation) {
+		this.organisation = organisation;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public boolean isLoginstatus() {
+		return loginstatus;
+	}
+
+	public void setLoginstatus(boolean loginstatus) {
+		this.loginstatus = loginstatus;
+	}
+
+	public String getSubmissionEmailID() {
+		return submissionEmailID;
+	}
+
+	public void setSubmissionEmailID(String submissionEmailID) {
+		this.submissionEmailID = submissionEmailID;
+	}
+
+	public String getSubmissionPassword() {
+		return submissionPassword;
+	}
+
+	public void setSubmissionPassword(String submissionPassword) {
+		this.submissionPassword = submissionPassword;
+	}
+
+	private String submissionPassword;
 
 	public SignUp() {
 
 	}
 
-	public SignUp(long id, String name, String email, String password,String phonenumber) {
-		super();
-		this.id = id;
-		this.name= name;
-		this.email = email;
-		this.password = password;
-		this.phoneNumber = phonenumber;
-	}
 
-	@Override
-	public String toString() {
-		return "{\"id\":" + id + ", \"name\":\"" + name + "\", \"email\":\"" + email + "\", \"phoneNumber\":\"" + phoneNumber
-				+ "\",\"password\":\""+ password +  "\"}";
-	}
+
+
 
 	public long getId() {
 		return id;
