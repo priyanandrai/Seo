a530b8d69233a02b45d1f8e85d9b343b6b9c8062
amitrai <amitrai5100@gmail.com>
2020-09-10 10:03:28 +0530

    ankur

--- frontend/src/maincontentpage2/maincontentpage2.jsx
+++ frontend/src/maincontentpage2/maincontentpage2.jsx
@@ -287,7 +287,9 @@ export class Maincontentpage2 extends Component {
       progressbar: true,
     });
     let month =  new Date().getMonth() +1;
+    if(month<10){month='0'+month}
     let day= new Date().getDate();
+     if(day<10){day='0'+day} 
     let year = new Date().getFullYear();
     let hour =  new Date().getHours();
     let min = new Date().getMinutes();
