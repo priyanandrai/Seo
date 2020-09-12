7ed34ed67d79326d03a7688798f02962c94f340c
amit rai <amitrai5100@gmail.com>
2020-09-12 16:26:22 +0530

    ujjawal

--- backend/seo/src/main/java/com/seo/model/SubmitRequest.java
+++ backend/seo/src/main/java/com/seo/model/SubmitRequest.java
@@ -1,5 +1,6 @@
 package com.seo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,52 +14,55 @@ public class SubmitRequest {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+	@Column(name="userid")
+	private Long userId;
 	private String email;
 	private String message;
 	
 	
-	public SubmitRequest(Long id, String email, String message) {
+	public SubmitRequest(Long id, Long userId, String email, String message) {
 		super();
 		this.id = id;
+		this.userId = userId;
 		this.email = email;
 		this.message = message;
 	}
-
-
+	
+	public SubmitRequest() {
+		
+	}
+	
 	@Override
 	public String toString() {
-		return "{\"id\":\"" + id + "\", \"email\":\"" + email + "\", \"message\":\"" + message + "\"}";
+		return "{\"id\":\"" + id + "\", \"userId\":\"" + userId + "\", \"email\":\"" + email + "\", \"message\":\"" + message + "\"}";
 	}
-
-
+	
+	
 	public Long getId() {
 		return id;
 	}
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
 	public String getEmail() {
 		return email;
 	}
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
 	public String getMessage() {
 		return message;
 	}
-
-
 	public void setMessage(String message) {
 		this.message = message;
 	}
+	
 	
 	
 	
