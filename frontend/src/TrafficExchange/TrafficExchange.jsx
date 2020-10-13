0a7da39d72a34d648f167793f2baa4cb632f3919
amitrai <amitrai5100@gmail.com>
2020-10-13 12:27:12 +0530

    shahsank

--- frontend/src/TrafficExchange/TrafficExchange.jsx
+++ frontend/src/TrafficExchange/TrafficExchange.jsx
@@ -9,6 +9,7 @@ import CardContent from "@material-ui/core/CardContent";
 import ReactPlayer from "react-player";
 import Dialog from "@material-ui/core/Dialog";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Dropdown } from 'react-bootstrap';
 import {
   faPencilAlt,
   faTimes,
@@ -37,40 +38,56 @@ class TrafficExchange extends React.Component {
   render() {
     return (
       <div className="mt-5">
-        <Grid container className="mt-5 submenu-alignment">
-        <Grid item md={3}></Grid>
-          <Grid item md={6}>
-            <div className="inputwidth">
-              <Form.Label>Url</Form.Label>
+      
+      <div className="traffic container">  <Grid className="mt-5">
+      
+         
+            <div className="inputtraffic ">
+              <Form.Label className="leftt mt-3">Enter Url</Form.Label>
               <Form.Control
                 type="name"
                 id="Name"
                 value={this.state.uid}
-                className="mobileviewset"
+                className="mt-3"
                 onChange={(e) => {
                   this.state = {
                     trafficdialog: true,
                   };
                 }}
-              />
+              />   <Form.Label className="leftt mt-3">Enter Time Limit</Form.Label><br/><br/>
+             <Dropdown className="leftt">
+  <Dropdown.Toggle variant="dark" id="dropdown-basic">
+   Time Limit
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
             </div>
            
-          </Grid>
-          <Grid item md={3}></Grid>
-        </Grid>
+         
+        
+        </Grid></div>
+     
         <div className="buttoncenter mb-5 mt-5">
              
               <Button
                 variant="contained"
                 className="startbtn"
                 onClick={this.trafficdialog}
-                // onClick={(e) => {
-                //   this.setState = {
-                //     trafficdialog:false
-                //   };
-                // }}
-              >
+               >
                 Start
+              </Button>
+              
+              <Button
+                variant="contained"
+                className="startbtn ml-5"
+                onClick={this.trafficdialog}
+               >
+               Stop
               </Button>
             </div>
 
