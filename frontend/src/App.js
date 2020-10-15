013f6baa83f956423dc6a510ef1d2d7f186dcfd4
amit rai <amitrai5100@gmail.com>
2020-10-15 10:50:16 +0530

    ujjawal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,25 @@
-import React, { Component, Fragment } from "react";
-import Nav from "./profile/nav";
+import React from "react";
 import Profile from "./profile/profile"
 import Tasks from "./profile/tasks"
 import Admin from "./profile/admin"
 import Admin_Dashboard from "./profile/admin_dashboard"
 import Admin_Profile from "./profile/admin_profile"
 import "./App.css";
-import HOC from "./components/HOC";
-import HOC1 from "./components/HOC1";
 import Maincontentpage from "./maincontentpage/maincontentpage";
-import Header from "./components/Header";
-import Login from "./components/login";
-import { Provider } from "react-redux";
-import Footer from "./components/Footer";
 import Logout from "./logout/logout";
-import Adminheader from "./profile/adminheader";
 import Userhistory from "./profile/userhistory";
 import TrafficExchange from "./TrafficExchange/TrafficExchange";
 import Yourwebsite from "./yourwebsite/yourwebsite"
-
-
 import {
   BrowserRouter as Router,
   Route,
-  Redirect,
-  Switch,
   BrowserRouter,
-  IndexRoute,
 } from "react-router-dom";
-import Leftpanel  from "./leftpanel/leftpanel";
 import  Maincontentpage2  from "./maincontentpage2/maincontentpage2";
 import { SubmitRequest } from "./submitRequest/submitRequest";
 import { Changepassword } from "./changepassword/changepassword";
 import { Confirmotp } from "./confirmotp/confirmotp";
-import { getAuthData, isLoggedIn } from "./utils";
+
 
 function App() {
  // if (!isLoggedIn()) {
