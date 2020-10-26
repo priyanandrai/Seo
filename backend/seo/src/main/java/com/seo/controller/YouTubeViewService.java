0d313f3145e11bb052f07d5bdf19193e44b9fe11
amit rai <amitrai5100@gmail.com>
2020-10-26 17:21:38 +0530

    ujjawal

--- backend/seo/src/main/java/com/seo/controller/YouTubeViewService.java
+++ backend/seo/src/main/java/com/seo/controller/YouTubeViewService.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.seo.model.TrafficExchange;
 import com.seo.model.YouTubeViews;
 import com.seo.services.IYouTubeViewsService;
 
@@ -44,4 +46,62 @@ public class YouTubeViewService {
 	}
 	
 	
+	@CrossOrigin(origins = "*")
+	@GetMapping("/getpersoninformation")
+	public String getpersoninformation(@RequestParam(defaultValue = "") String url ) {
+		try {
+			Iterator<YouTubeViews> iterable = iYouTubeViewsService.findAll().iterator();
+			while(iterable.hasNext()){
+				YouTubeViews youTubeViews = iterable.next();
+				if(youTubeViews.getUrl() != null && youTubeViews.getUrl().equalsIgnoreCase(url)) {
+					return youTubeViews.toString();
+				}
+			}
+			return "{\"messgae\":\"No information exist\"}";
+		} catch (Exception e) {
+			return "{\"messgae\":\"Sorry . something worng went worng on your end\"}";
+		}
+	}
+	
+	@CrossOrigin(origins = "*")
+	@GetMapping("/updateCountviews")
+	public String updateCountviews(@RequestParam(defaultValue = "") String url ) {
+		try {
+			Iterator<YouTubeViews> iterable = iYouTubeViewsService.findAll().iterator();
+			while(iterable.hasNext()){
+				YouTubeViews youTubeViews = iterable.next();
+				if(youTubeViews.getUrl() != null && youTubeViews.getUrl().equalsIgnoreCase(url)) {
+					youTubeViews.setRemaining_balance_views(youTubeViews.getRemaining_balance_views()+1);
+					 iYouTubeViewsService.save(youTubeViews);
+					 break;
+				}
+			}
+			return "{\"messgae\":\"Views Updated Sucessfully\"}";
+		} catch (Exception e) {
+			return "{\"messgae\":\"Sorry . something worng went worng on your end\"}";
+		}
+	}
+	
+	
+	@CrossOrigin(origins = "*")
+	@GetMapping("/updatearchivedviews")
+	public String updatearchivedhit(@RequestParam(defaultValue = "") String url ) {
+		try {
+			Iterator<YouTubeViews> iterable = iYouTubeViewsService.findAll().iterator();
+			while(iterable.hasNext()){
+				YouTubeViews youTubeViews = iterable.next();
+				System.out.println("youTubeViews.getUrl() "+ youTubeViews.getUrl());
+				System.out.println("url "+ url);
+				if(youTubeViews.getUrl() != null && youTubeViews.getUrl().equalsIgnoreCase(url)) {
+					youTubeViews.setArchieved_views(youTubeViews.getArchieved_views()+1);
+					youTubeViews.setRemaining_balance_views(youTubeViews.getRemaining_balance_views()-1);
+					iYouTubeViewsService.save(youTubeViews);
+					 break;
+				}
+			}
+			return "{\"messgae\":\"Views Updated Sucessfully\"}";
+		} catch (Exception e) {
+			return "{\"messgae\":\"Sorry . something worng went worng on your end\"}";
+		}
+	}
 }
