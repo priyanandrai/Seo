b487b011ec27512044f90f60539f64fbc4f30779
amitrai <amitrai5100@gmail.com>
2020-11-04 16:31:14 +0530

    ankur

--- frontend/src/timestamp/timestamp_converter.jsx
+++ frontend/src/timestamp/timestamp_converter.jsx
@@ -17,177 +17,252 @@ import "./timpstamp-converter.css";
 class Timestamp_converter extends React.Component {
   constructor(props) {
     super(props);
-this.Timestamptodate=this.Timestamptodate.bind(this);
-this.datetotimestamp=this.datetotimestamp.bind(this);
+    this.Timestamptodate = this.Timestamptodate.bind(this);
+    this.datetotimestamp = this.datetotimestamp.bind(this);
     this.state = {
-      timestap:"N/A",
-      datetotime:"N/A",
-      dateupdate:"",
-      timestamp: 1578162600000
+      timestap: "N/A",
+      datetotime: "N/A",
+      inputtimestamp: "",
+      dateupdate: "",
+      dateupdate1: "",
+      dateupdate2: "",
+      dateupdate3: "",
+      dateupdate4: "",
+      dateupdate5: "",
+      dateupdate6: "",
+      timestamp1: "N/A",
+      timestamp: 1578162600000,
     };
   }
-  Timestamptodate(){
-    if (this.state.timestamp === "") {
-        this.setState({
-          snackbar: true,
-          error: "please insert date",
-        });
-        return;
-      }
-      // if (this.state.timestamp === timestamp) {
-      //   this.setState({
-      //     snackbar: true,
-      //     error: "dsfsd",
-      //   });
-      //   return;
-      // }
-    // let btnGet = document.getElementById('timebutton');
-    let result = document.getElementById('resulttime');
-     
-    //1/5/2020
-  let temp =this.state.timestamp;
-     
-    // btnGet.addEventListener('click', () => {
-        let dateObj = new Date(temp);
-     
-        let month = dateObj.getMonth() + 1;
-        if (month < 10) {
-          month = "0" + month;
-        }
-        let year = dateObj.getFullYear();
-        let date = dateObj.getDate();
-        if (date < 10) {
-          date = "0" + date;
-        }
-        let hour = dateObj.getHours();
-        if (hour < 10) {
-          hour = "0" + hour;
-        }
-        let min =dateObj.getMinutes();
-        if (min < 10) {
-          min = "0" + min;
-        }
-        let sec =dateObj.getSeconds();
-        if (sec < 10) {
-          sec = "0" + sec;
-        }
-     
-        result.innerText = `${month}-${date}-${year}`+" "+`${hour}:${min}:${sec}`;
+  Timestamptodate() {
+    if (this.state.inputtimestamp === "") {
+      this.setState({
+        snackbar: true,
+        error: "please insert Timestamp",
+      });
+      return;
+    }
+
+    let result = document.getElementById("resulttime");
+
+    let temp = this.state.timestamp;
+
+    let dateObj = new Date(temp);
+
+    let month = dateObj.getMonth() + 1;
+    if (month < 10) {
+      month = "0" + month;
+    }
+    let year = dateObj.getFullYear();
+    let date = dateObj.getDate();
+    if (date < 10) {
+      date = "0" + date;
+    }
+    let hour = dateObj.getHours();
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    let min = dateObj.getMinutes();
+    if (min < 10) {
+      min = "0" + min;
+    }
+    let sec = dateObj.getSeconds();
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+
+    result.innerText =
+      `${month}-${date}-${year}` + " " + `${hour}:${min}:${sec}`;
     // });
   }
-  // componentDidMount(){
-
-  //   let datennn=Math.floor(new Date().getTime()/1000.0);
-
-      
-  // }
-  datetotimestamp(){
-
-    // alert("Hii")
-    // let a=this.state.dateupdate;
-    //  alert(a)
-
-//     var dateString = a,
-//     dateTimeParts = dateString.split(' '),
-//     timeParts = dateTimeParts[1].split(':'),
-//     dateParts = dateTimeParts[0].split('-'),
-//     date;
-
-// date = new Date(dateParts[2], parseInt(dateParts[1], 10) - 1, dateParts[0], timeParts[0], timeParts[1]);
-
-// console.log(date.getTime()); //1379426880000
-// console.log(date); 
-// new Date().getTime()/1000;
-// alert(date)
-//////////alert(	Math.floor(new Date().getTime()/1000.0));
-
-    // console.log("r54534",Math.floor(new Date().getTime()/1000.0))
-    // let datetotime= Math.floor(new Date().getTime()/1000.0)
+  componentDidMount() {}
+  datetotimestamp() {
     // if (this.state.dateupdate === "") {
     //   this.setState({
     //     snackbar: true,
-    //     error: "please insert date",
+    //     error: "please insert Date and Time",
     //   });
     //   return;
     // }
-    // if (this.state.dateupdate === datetotime) {
-    //   this.setState({
-    //     snackbar: true,
-    //     error: "dsfsd",
-    //   });
-    //   return;
-    // }
-
-   
-   
+    let today = new Date(this.state.dateupdate);
+    let timestamp = today.valueOf();
+    console.log(timestamp, "Today");
+    this.setState({
+      timestamp1: timestamp.toString(),
+    });
   }
   closeSnackbar = () => {
     this.setState({ snackbar: false });
   };
 
   render() {
-//     const unixTime = 1210981217;
-// const date = new Date(unixTime*1000);
-// console.log(date.toLocaleDateString("en-US"));
-//expected: "5/16/2008"
+    const fullYear = new Date().getFullYear();
+    let month = new Date().getMonth() + 1;
+    if (month < 10) {
+      month = "0" + month;
+    }
+    let date = new Date().getDate();
+    if (date < 10) {
+      date = "0" + date;
+    }
+    let hours = new Date().getHours();
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    let min = new Date().getMinutes();
+    if (min < 10) {
+      min = "0" + min;
+    }
+    let sec = new Date().getSeconds();
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
     return (
       <div>
-        <p className="time_stamp_para"> Epoch & Unix Timestamp Conversion Tools</p>
+        <p className="time_stamp_para">
+          {" "}
+          Epoch & Unix Timestamp Conversion Tools
+        </p>
         <Grid className=" mywebsite_container">
           <Grid item md={6}>
-          <div className="time_stamp mt-5 mb-5">
-            <p className="time_convert">Convert timestamp to date</p>
-          <Form.Control
-            type="name"
-            placeholder="Time stamp"
-            id="Name"
-            className="time_width"
-            value={this.state.timestamp}
-            onChange={(e) => this.setState({ timestamp: e.target.value })}
-          />
-        </div>
-        <div className="timestamp_btn mb-5">
-          <Button id="timebutton"
-            variant="contained"
-            className="timestamp_btn_color"
-            onClick={this.Timestamptodate}
-          >
-            Converter
-          </Button>
-        </div>
-        <div className="ml-5">
-         <p id="resulttime">{this.state.timestap}</p>
-        </div>
-        </Grid>
-        <Grid item md={6}>
-           
-        <div className="time_stamp mt-5 mb-5">
-        <p className="time_convert">Convert date to timestamp</p>
-          <Form.Control
-            type="name"
-            placeholder="Time stamp"
-            id="Name"
-            className="time_width"
-            value={this.state.dateupdate}
-            onChange={(e) => this.setState({ dateupdate: e.target.value })}
-          />
-        </div>
-        <div className="timestamp_btn mb-5">
-          <Button
-            variant="contained"
-            className="timestamp_btn_color"
-            onClick={this.datetotimestamp}
-          >
-            Converter
-          </Button>
-        </div>
-        <div>
-            <p>{this.state.datetotime}</p>
-          </div>
-        </Grid>
+            <div className="time_stamp mt-5 mb-5">
+              <p className="time_convert">Convert timestamp to date</p>
+              <Form.Control
+                type="name"
+                placeholder="Time stamp"
+                id="Name"
+                className="time_width"
+                value={this.state.inputtimestamp}
+                onChange={(e) =>
+                  this.setState({ inputtimestamp: e.target.value })
+                }
+              />
+            </div>
+            <div className="timestamp_btn mb-5">
+              <Button
+                id="timebutton"
+                variant="contained"
+                className="timestamp_btn_color"
+                onClick={this.Timestamptodate}
+              >
+                Converter
+              </Button>
+            </div>
+            <div className="ml-5">
+              <p id="resulttime">{this.state.timestap}</p>
+            </div>
           </Grid>
-          
-      
+          <Grid item md={6}>
+            <div className="time_stamp mt-5 mb-5">
+              <p className="time_convert">Convert date to timestamp</p>
+              <div className="d-flex">
+                <span className="year_span_width">
+                  <Form.Label>Year</Form.Label>
+                  <Form.Control
+                    type="name"
+                    id="Name"
+                    className="year_width"
+                    placeholder={fullYear}
+                    value={this.state.dateupdate1}
+                    onChange={(e) =>
+                      this.setState({ dateupdate1: e.target.value })
+                    }
+                  />
+                </span>
+                <span className="year_span_width ml-1">
+                  <Form.Label>Mon</Form.Label>
+
+                  <Form.Control
+                    type="name"
+                    placeholder={month}
+                    id="Name"
+                    className="year_width1"
+                    value={this.state.dateupdate2}
+                    onChange={(e) =>
+                      this.setState({ dateupdate2: e.target.value })
+                    }
+                  />
+                </span>
+                <span className="year_span_width ml-1">
+                  <Form.Label>Date</Form.Label>
+
+                  <Form.Control
+                    type="name"
+                    placeholder={date}
+                    id="Name"
+                    className="year_width1"
+                    value={this.state.dateupdate3}
+                    onChange={(e) =>
+                      this.setState({ dateupdate3: e.target.value })
+                    }
+                  />
+                </span>
+                <span className="year_span_width ml-1">
+                  <Form.Label>Hr</Form.Label>
+
+                  <Form.Control
+                    type="name"
+                    placeholder={hours}
+                    id="Name"
+                    className="year_width1"
+                    value={this.state.dateupdate4}
+                    onChange={(e) =>
+                      this.setState({ dateupdate4: e.target.value })
+                    }
+                  />
+                </span>
+                <span className="year_span_width ml-1">
+                  <Form.Label>Min</Form.Label>
+
+                  <Form.Control
+                    type="name"
+                    placeholder={min}
+                    id="Name"
+                    className="year_width1"
+                    value={this.state.dateupdate5}
+                    onChange={(e) =>
+                      this.setState({ dateupdate5: e.target.value })
+                    }
+                  />
+                </span>
+                <span className="year_span_width ml-1">
+                  <Form.Label>Sec</Form.Label>
+
+                  <Form.Control
+                    type="name"
+                    placeholder={sec}
+                    id="Name"
+                    className="year_width1"
+                    value={this.state.dateupdate6}
+                    onChange={(e) =>
+                      this.setState({ dateupdate6: e.target.value })
+                    }
+                  />
+                </span>
+              </div>
+              {/* <Form.Control
+                type="name"
+                placeholder="Time stamp"
+                id="Name"
+                className="time_width"
+                value={this.state.dateupdate}
+                onChange={(e) => this.setState({ dateupdate: e.target.value })}
+              /> */}
+            </div>
+            <div className="timestamp_btn mb-5">
+              <Button
+                variant="contained"
+                className="timestamp_btn_color"
+                onClick={this.datetotimestamp}
+              >
+                Converter
+              </Button>
+            </div>
+            <div>
+              <p>{this.state.timestamp1}</p>
+            </div>
+          </Grid>
+        </Grid>
 
         <div>
           <Snackbar
